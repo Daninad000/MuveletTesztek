3ab3187772230eb1c1ecc914be51e35158e5d790
Daninad000 <daniel.szabo.000@gmail.com>
2022-02-24 13:17:57 +0100

    Add files via upload

---
+++ MuveletTesztek/MuveletTesztek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Muveletek;
+using NUnit.Framework;
+
+namespace MuveletTesztek
+{
+    [TestFixture]
+    public class MuveletTesztek
+    {
+        static void Main(string[] args)
+        {
+            Osszeadas a = new Osszeadas("", "222");
+            Console.WriteLine(a.osszeadMaskepp());
+            Kivonas k = new Kivonas("", "");
+            Console.WriteLine(k.kivon());
+            Console.ReadKey();
+
+
+        }
+    }
+}
